070739eea078a2e2abf7a0f0c992b10c71a9e136
Elena1118 <elenaherrerafernandez1196@gmail.com>
2024-10-27 22:53:10 +0000

    imagenes con linck de navbar wrapper funcionando en Best sellers (terminar de ver el css )

--- src/App.js
+++ src/App.js
@@ -88,6 +88,24 @@ function App() {
                     <li><a href="#lipline">Lip Liner</a></li>
                     <li><a href="#lipprimer">Lip Primer</a></li>
                   </ul>
+                   
+      {/* Contenedor de imágenes con descripciones a la derecha */}
+      <div className="subcategory-links">
+        <div className="link-item">
+          <a href="#link1">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/MAQUILLAJE.jpg" alt="Halloween Makeup Sets" />
+            <h3>Makeup de verano</h3>
+            <p>Aprende a maquillarte cuando te ha dado el sol.</p>
+          </a>
+        </div>
+        <div className="link-item">
+          <a href="#link2">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/MAQUILLAJE.jpg" alt="New M·A·C ximal Sleek Satin Lipstick" />
+            <h3>New makeup de Elena</h3>
+            <p>Aprende a maquillarte con Elena paso a paso.</p>
+          </a>
+        </div>
+      </div>
                 </div>
               )}
             </li>
