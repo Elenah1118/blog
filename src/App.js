6964e96eebc2ab754cb6710b63402e239cf31471
Elena1118 <elenaherrerafernandez1196@gmail.com>
2024-10-28 21:30:11 +0000

    carrusel funcionando pero estamos en proceso de arreglar los puntos de abajo de este, que no terminan rellenar el circulo

--- src/App.js
+++ src/App.js
@@ -250,7 +250,18 @@ function App() {
           <button className="carousel-button right" onClick={handleNextImage}>
             &#9654; {/* Flecha hacia la derecha */}
           </button>
+          {/* Indicadores de puntos */}
+          <div className="carousel-indicators">
+            {images.map((_, index) => (
+              <div
+                key={index}
+                className={`indicator ${currentImageIndex === index ? 'active' : ''}`}
+                onClick={() => setCurrentImageIndex(index)}
+              ></div>
+            ))}
+          </div>
         </div>
+        
         <h1>Bienvenida a tu blog de maquillaje</h1>
         <p>Este es el contenido inicial de tu blog. Continuaremos añadiendo más elementos y diseño.</p>
       </section>
